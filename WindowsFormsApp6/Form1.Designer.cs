0e7651c405d50f3f8583a4f21191e9b9881ed1bc
Klubin <clubin2004@gmail.com>
2023-12-18 18:40:08 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTargetFolder = new System.Windows.Forms.TextBox();
+            this.textBoxCopyFolder = new System.Windows.Forms.TextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listBoxForbiddenWords = new System.Windows.Forms.ListBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.buttonTarget = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxTargetFolder
+            // 
+            this.textBoxTargetFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBoxTargetFolder.Location = new System.Drawing.Point(12, 12);
+            this.textBoxTargetFolder.Name = "textBoxTargetFolder";
+            this.textBoxTargetFolder.Size = new System.Drawing.Size(450, 44);
+            this.textBoxTargetFolder.TabIndex = 0;
+            // 
+            // textBoxCopyFolder
+            // 
+            this.textBoxCopyFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBoxCopyFolder.Location = new System.Drawing.Point(610, 12);
+            this.textBoxCopyFolder.Name = "textBoxCopyFolder";
+            this.textBoxCopyFolder.Size = new System.Drawing.Size(542, 44);
+            this.textBoxCopyFolder.TabIndex = 1;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(12, 62);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(223, 60);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "buttonLoad";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(241, 62);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(221, 62);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "buttonStart";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listBoxForbiddenWords
+            // 
+            this.listBoxForbiddenWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.listBoxForbiddenWords.FormattingEnabled = true;
+            this.listBoxForbiddenWords.ItemHeight = 32;
+            this.listBoxForbiddenWords.Location = new System.Drawing.Point(12, 130);
+            this.listBoxForbiddenWords.Name = "listBoxForbiddenWords";
+            this.listBoxForbiddenWords.Size = new System.Drawing.Size(450, 708);
+            this.listBoxForbiddenWords.TabIndex = 4;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 850);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(1274, 23);
+            this.progressBar.TabIndex = 5;
+            // 
+            // buttonTarget
+            // 
+            this.buttonTarget.Location = new System.Drawing.Point(468, 12);
+            this.buttonTarget.Name = "buttonTarget";
+            this.buttonTarget.Size = new System.Drawing.Size(136, 44);
+            this.buttonTarget.TabIndex = 6;
+            this.buttonTarget.Text = "Target Folder";
+            this.buttonTarget.UseVisualStyleBackColor = true;
+            this.buttonTarget.Click += new System.EventHandler(this.buttonTarget_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(1158, 12);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(128, 44);
+            this.buttonCopy.TabIndex = 7;
+            this.buttonCopy.Text = "Copy Folder";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(610, 130);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(676, 708);
+            this.dataGridView.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 885);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.buttonTarget);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.listBoxForbiddenWords);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.textBoxCopyFolder);
+            this.Controls.Add(this.textBoxTargetFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTargetFolder;
+        private System.Windows.Forms.TextBox textBoxCopyFolder;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listBoxForbiddenWords;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonTarget;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        private List<string> forbiddenWords = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            buttonLoad.Enabled = true;
+            buttonStart.Enabled = false;
+            textBoxTargetFolder.Text = "C:\\Users\\" + Environment.UserName + "\\Desktop";
+            textBoxCopyFolder.Text = "C:\\Users\\" + Environment.UserName + "\\Desktop\\test";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<string> GetForbiddenWords()
+        {
+            return forbiddenWords;
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            listBoxForbiddenWords.Items.Clear();
+            forbiddenWords = new List<string>();
+            // Открываем диалоговое окно выбора файла
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Читаем файл с запрещенными словами
+                using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        forbiddenWords.Add(reader.ReadLine());
+                    }
+                }
+                // Обновляем список запрещенных слов
+                for (int i = 0; i < forbiddenWords.Count; i++)
+                {
+                    listBoxForbiddenWords.Items.Add(forbiddenWords[i]);
+                }
+            }
+            buttonStart.Enabled = true;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            progressBar.Maximum = 100;
+            progressBar.Step = 1;
+            progressBar.Value = 1;
+
+            // Получаем пути к папкам с исходными и целевыми файлами
+            string targetFolderPath = textBoxTargetFolder.Text;
+            string copyFolderPath = textBoxCopyFolder.Text;
+            progressBar.Value = 5;
+
+            // Создаем объект BackgroundWorker
+            BackgroundWorker worker = new BackgroundWorker();
+
+            // Устанавливаем свойство WorkerReportsProgress в true
+            worker.WorkerReportsProgress = true;
+            progressBar.Value = 15;
+
+            progressBar.Value = 25;
+            // Задаем обработчик завершения работы
+            worker.DoWork += (o, args) =>
+            {
+                // Проходим по всем файлам в папке с исходными файлами
+                foreach (string filePath in Directory.GetFiles(targetFolderPath))
+                {
+                    // Проверяем, содержит ли файл запрещенные слова
+                    bool containsForbiddenWords = ContainsForbiddenWords(filePath);
+
+                    // Если файл содержит запрещенные слова, копируем его в папку с целевыми файлами
+                    if (containsForbiddenWords)
+                    {
+                        // Копируем исходный файл
+                        CopyFile(filePath, Path.Combine(copyFolderPath, Path.GetFileName(filePath)));
+
+                        // Создаем новый файл с замененными запрещенными словами
+                        string newFilePath = Path.Combine(copyFolderPath, Path.GetFileNameWithoutExtension(filePath) + "_censored.txt");
+                        ReplaceForbiddenWords(filePath, newFilePath);
+
+                        // Обновляем индикатор прогресса
+                        int progress = 50;
+                        progressBar.Value = progress;
+                    }
+                }
+
+                // Уведомляем основной поток о завершении работы
+                worker.ReportProgress(100);
+            };
+
+            // Задаем обработчик прогресса
+            worker.ProgressChanged += (o, args) =>
+            {
+                // Обновляем индикатор прогресса
+                progressBar.Value = (int)args.ProgressPercentage;
+            };
+
+            // Запускаем фоновую задачу
+        }
+        private bool ContainsForbiddenWords(string filePath)
+        {
+            // Читаем содержимое файла
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                // Проходим по всем словам в файле
+                while (!reader.EndOfStream)
+                {
+                    string word = reader.ReadLine();
+
+                    // Проверяем, содержится ли слово в списке запрещенных слов
+                    foreach (string forbiddenWord in forbiddenWords)
+                    {
+                        if (word.Equals(forbiddenWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private void ReplaceForbiddenWords(string filePath, string newFilePath)
+        {
+            // Читаем содержимое файла
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                // Создаем новый файл
+                using (StreamWriter writer = new StreamWriter(newFilePath))
+                {
+                    // Проходим по всем словам в файле
+                    while (!reader.EndOfStream)
+                    {
+                        string word = reader.ReadLine();
+
+                        // Если слово является запрещенным, заменяем его на 7 звезд
+                        if (forbiddenWords.Contains(word))
+                        {
+                            writer.WriteLine("*******");
+                        }
+                        else
+                        {
+                            writer.WriteLine(word);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CopyFile(string filePath, string newFilePath)
+        {
+            // Создаем новый файл
+            using (FileStream newFile = new FileStream(newFilePath, FileMode.Create))
+            {
+                // Копируем содержимое исходного файла в новый файл
+                using (FileStream oldFile = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+
+                    while ((bytesRead = oldFile.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        newFile.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+
+        private void buttonTarget_Click(object sender, EventArgs e)
+        {
+            // Открываем диалоговое окно выбора папки
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.SelectedPath = @textBoxTargetFolder.Text;
+            folderBrowserDialog.Description = "Выберите папку с исходными файлами";
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Задаем путь к папке в текстовом поле
+                textBoxTargetFolder.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            // Открываем диалоговое окно выбора папки
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.SelectedPath = @textBoxCopyFolder.Text;
+            folderBrowserDialog.Description = "Выберите папку для сохранения копий";
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Задаем путь к папке в текстовом поле
+                textBoxCopyFolder.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+    }
+}
